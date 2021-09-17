be87e97bb235f5618a7b7c7b6b1750d7dd2eeac6
Roman Ciobanu <roman.ciobanu@saltedge.com>
2021-09-17 23:28:39 +0300

    update header

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,11 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'home/about', as: "about"
   get 'home/compare', as: "compare"
+  post 'home/compare', as: "compare_post"
   get 'home/addcompetition', as: "addcompetition"
   get 'home/add_competition_file', as: "competition_file"
   post 'home/add_competition_file', as: "competition_file_post"
   get 'home/add_runners_file', as: "runner_file"
   post 'home/add_runners_file', as: "runner_file_post"
-  post 'home/compare', as: "compare_post"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
